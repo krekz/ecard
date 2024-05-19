5e78d36813ab88850bb83020807bda8eaac2a5dc
kyrool <kfitri559@gmail.com>
2024-05-20 02:33:04 +0800

    :wastedbasket:

---
+++ next.config.js
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: [
+      "lh3.googleusercontent.com",
+      "images.unsplash.com",
+      "randomuser.me",
+      "bkduabhaudrkgjloqnck.supabase.co",
+    ],
+  },
+};
+
+export default nextConfig;
