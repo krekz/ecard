1b64bb6b2bb48b2ba1ccc90726cf4a165be0266b
kyrool <kfitri559@gmail.com>
2024-05-20 03:01:37 +0800

    :recycle: make reusable to get all designs

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,7 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
 import {  primaryFontsConst , secondaryFontsConst } from "./constant"
+import prisma from "../../prisma"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
@@ -9,4 +10,18 @@ export function cn(...inputs: ClassValue[]) {
 export function getFont(id: string) {
   return primaryFontsConst.find((font) => font.id === id) || secondaryFontsConst.find((font) => font.id === id)
 }
+
+export const getAllDesigns = async () => {
+  const cards = await prisma.design.findMany({
+    select: {
+      designId: true,
+      category: true,
+      name: true,
+      thumbnail: true,
+      front_design_url:true,
+    },
+  });
+
+  return cards;
+};
 
