9833e3385d6c03a9b40b81e7d5a3c7b558ff4c63
kyrool <kfitri559@gmail.com>
2024-05-22 04:04:59 +0800

    🦺 add 'update design' actions

--- actions/admin/admin-actions.ts
+++ actions/admin/admin-actions.ts
@@ -1,20 +1,19 @@
 "use server";
 import { auth } from "@/auth";
 import { createClient } from "@/lib/supabase/server";
-import { redirect } from "next/navigation";
-import { NextResponse } from "next/server";
 import { v4 as uuidv4 } from "uuid";
 import prisma from "../../prisma";
 import {
-  upsertDesignSchema,
   deleteDesignSchema,
+  updateDesignSchema,
+  uploadDesignSchema,
 } from "../../schema/zod/admin-form";
 import { revalidatePath } from "next/cache";
 
 export const uploadDesign = async (formData: FormData) => {
   const values = Object.fromEntries(formData.entries());
   const { category, design_name, front_design, content_design, thumbnail } =
-    upsertDesignSchema.parse(values);
+    uploadDesignSchema.parse(values);
 
   try {
     const session = await auth();
@@ -92,24 +91,13 @@ export const uploadDesign = async (formData: FormData) => {
   }
 };
 
-export const updateDesign = async (formData: FormData) => {
-  // INCOMPLETE
-  const values = Object.fromEntries(formData.entries());
-  const { category, design_name, thumbnail, front_design, content_design } =
-    upsertDesignSchema.parse(values);
-  try {
-    const session = await auth();
-    if (!session) throw new Error("Unauthorized access");
-  } catch (error) {
-    console.error(error);
-  }
-};
-
 export const deleteDesign = async (formData: FormData) => {
   const values = Object.fromEntries(formData.entries());
+  console.log(values);
   const { choose_design } = deleteDesignSchema.parse(values);
   const supabase = createClient();
   const chooseDesign = choose_design.toLowerCase();
+  console.log(chooseDesign);
   try {
     const session = await auth();
     if (!session) throw new Error("Unauthorized access");
@@ -120,10 +108,13 @@ export const deleteDesign = async (formData: FormData) => {
       },
     });
 
-    if (!design) throw new Error("Design not found");
+    if (!design)
+      return {
+        ok: false,
+        message: "Design not found",
+      };
 
     // TODO : Role admin
-
     const { data: list } = await supabase.storage
       .from("e-card bucket")
       .list(`design/${chooseDesign}`);
@@ -144,9 +135,97 @@ export const deleteDesign = async (formData: FormData) => {
     await prisma.design.delete({
       where: { designId: chooseDesign },
     });
+
+    revalidatePath("/user/cards");
+    revalidatePath("/catalog");
+    revalidatePath("/auth/admin/upload-design");
+    return {
+      ok: true,
+      message: "Design deleted successfully",
+    };
   } catch (error) {
     console.log(error);
+    return {
+      ok: false,
+      message: "Error deleting design",
+    };
   }
-  revalidatePath("/user/cards");
-  revalidatePath("/auth/admin/upload-design");
+};
+
+export const updateDesign = async (formData: FormData, designName: string) => {
+  // TODO: admin role
+  const values = Object.fromEntries(formData.entries());
+  const { category, design_name, front_design, content_design, thumbnail } =
+    updateDesignSchema.parse(values);
+  const newDesignName = design_name.toUpperCase();
+  const design_LOWERCASE = designName.toLowerCase();
+
+  try {
+    const session = await auth();
+    if (!session) throw new Error("Unauthorized access");
+
+    const design = await prisma.design.findUnique({
+      where: {
+        designId: design_LOWERCASE,
+      },
+    });
+
+    if (!design)
+      return {
+        ok: false,
+        message: "Design not found",
+      };
+    const supabase = createClient();
+
+    const images = { thumbnail, front_design, content_design };
+    let imagesUrl: { [key: string]: string } = {};
+
+    for (const [key, value] of Object.entries(images)) {
+      if (value) {
+        const { data, error } = await supabase.storage
+          .from("e-card bucket")
+          .upload(`design/${design_LOWERCASE}/${key}-${uuidv4()}`, value as File);
+
+        if (data) {
+          imagesUrl[key] = data.path;
+        } else {
+          console.log(error);
+          return {
+            ok: false,
+            message: "Error uploading images",
+          };
+        }
+      }
+    }
+
+    const updateData: { [key: string]: any } = {
+      category,
+      name: newDesignName,
+    };
+
+    if (imagesUrl.thumbnail) updateData.thumbnail = imagesUrl.thumbnail;
+    if (imagesUrl.front_design)
+      updateData.front_design_url = imagesUrl.front_design;
+    if (imagesUrl.content_design)
+      updateData.content_design_url = imagesUrl.content_design;
+
+    await prisma.design.update({
+      where: {
+        designId: design_LOWERCASE,
+      },
+      data: updateData,
+    });
+
+    revalidatePath("/auth/admin/upload-design");
+    return {
+      ok: true,
+      message: "Design updated successfully",
+    };
+  } catch (error) {
+    console.error(error);
+    return {
+      ok: false,
+      message: "Error updating design",
+    };
+  }
 };
--- schema/zod/admin-form.ts
+++ schema/zod/admin-form.ts
@@ -1,6 +1,6 @@
-import { z } from "zod";
+import * as z from "zod";
 
-const fileFormat = z
+const OPTIONAL_IMAGE = z
   .custom<File | undefined>()
   .refine(
     (file) => !file || (file instanceof File && file.type.startsWith("image/")),
@@ -10,15 +10,36 @@ const fileFormat = z
     return !file || file.size < 1024 * 1024 * 2;
   }, "File must be less than 2MB");
 
-export const upsertDesignSchema = z.object({
+const REQUIRED_IMAGE = z
+  .custom<File | undefined>()
+  .refine(
+    (file) => file && file instanceof File && file.type.startsWith("image/"),
+    "Must be an image file"
+  )
+  .refine((file) => {
+    return file && file.size < 1024 * 1024 * 2;
+  }, "File must be less than 2MB");
+
+export const uploadDesignSchema = z.object({
   design_name: z
     .string()
     .min(2, "Design name is required")
     .transform((str) => str.replace(/\s+/g, "")),
   category: z.string().min(2, "Category is required"),
-  thumbnail : fileFormat,
-  front_design: fileFormat,
-  content_design: fileFormat,
+  thumbnail: REQUIRED_IMAGE,
+  front_design: REQUIRED_IMAGE,
+  content_design: REQUIRED_IMAGE,
+});
+
+export const updateDesignSchema = z.object({
+  design_name: z
+    .string()
+    .min(2, "Design name is required")
+    .transform((str) => str.replace(/\s+/g, "")),
+  category: z.string().min(2, "Category is required"),
+  thumbnail: OPTIONAL_IMAGE,
+  front_design: OPTIONAL_IMAGE,
+  content_design: OPTIONAL_IMAGE,
 });
 
 export const deleteDesignSchema = z.object({
@@ -26,7 +47,10 @@ export const deleteDesignSchema = z.object({
 });
 
 export const createVoucherFormSchema = z.object({
-  code : z.string().min(3,"Voucher must be more than 3 characters").max(5,"Only 5 characters allowed"),
-  description : z.string().min(2,"Description required"),
+  code: z
+    .string()
+    .min(3, "Voucher must be more than 3 characters")
+    .max(5, "Only 5 characters allowed"),
+  description: z.string().min(2, "Description required"),
   max_claims: z.coerce.number(),
 });
