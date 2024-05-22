c0c241c09689cebc2f3180ada1014f78b0cb650c
kyrool <kfitri559@gmail.com>
2024-05-22 16:55:51 +0800

    🚸 add loading state

---
+++ src/app/(root)/loading.tsx
@@ -0,0 +1,11 @@
+import Spinner from "@/components/admin/spinner";
+
+const HomeLoading = () => {
+  return (
+    <div className="flex justify-center items-center h-screen">
+        <Spinner />
+    </div>
+  )
+}
+
+export default HomeLoading
