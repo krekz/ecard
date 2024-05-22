88812046a2d37409609be12f1e049128dd597fcc
kyrool <kfitri559@gmail.com>
2024-05-22 16:56:39 +0800

    ♻️ refactor to separate component

--- src/app/(root)/catalog/page.tsx
+++ src/app/(root)/catalog/page.tsx
@@ -1,116 +1,22 @@
-import Link from "next/link";
-import Image from "next/image";
-import { Card, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
-import { buttonVariants } from "@/components/ui/button";
-import { cardList, checkboxList } from "@/lib/constant";
-import { cn } from "@/lib/utils";
-import CatalogFilter from "@/components/catalog-filter";
-import { getAllDesigns } from "@/lib/utils";
-
-type TCards = {
-  designId: string;
-  category: string;
-  name: string;
-  thumbnail: string;
-};
+import { Suspense } from "react";
+import HomeLoading from "@/app/(root)/loading";
+import CatalogCard from "@/components/catalog-card";
 
 const CatalogPage = async ({
   searchParams,
 }: {
   searchParams: { [key: string]: string | string[] | undefined };
 }) => {
-  const getFilteredCards = async () => {
-    const cards = await getAllDesigns();
-    let filteredCards = cards;
-
-    // sort based on 'filter' query parameter
-    const filters = searchParams.filter;
-    if (filters) {
-      const filterArrayParams = Array.isArray(filters) ? filters : [filters]; // convert the 'filter' params to an array if it's not and vice versa
-      filteredCards = filteredCards.filter((card) =>
-        filterArrayParams.some((filter) =>
-          card.category.toLowerCase().includes(filter.toLowerCase())
-        )
-      );
-    }
-
-    // sorting based on 'sortBy' query parameter
-    const sortBy = searchParams.sortBy as string;
-    // switch (sortBy) {
-    //   case "popular":
-    //     filteredCards = filteredCards.filter((card) => card.isPopular);
-    //     break;
-    //   case "newest":
-    //     filteredCards = filteredCards.filter((card) => card.isNewest);
-    //     break;
-    //   default: // no sorting applied
-    //     break;
-    // }
-
-    return filteredCards;
-  };
-
-  const filtered: TCards[] = await getFilteredCards();
-
   return (
     <main className="p-24 sm:container flex flex-col flex-wrap">
       <h1 className="text-center text-4xl font-bold uppercase">
         Browse E-card
       </h1>
-      <div className="flex flex-col md:flex-row">
-        {/* Filter */}
-        <aside className="p-3 w-52 lg:w-52 lg:bl1ock">
-          <CatalogFilter checkboxList={checkboxList} />
-        </aside>
-        {/* Card Listing */}
-        <section className="p-3 w-full mx-auto ">
-          <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2 ">
-            {filtered.map((card, index) => (
-              <Card
-                key={index}
-                className={cn(
-                  "text-center relative "
-                  // card.isPopular &&
-                  //   "before:content-['Popular'] before:bg-yellow-400 before:absolute before:-left-5 before:-top-2 before:-rotate-[20deg]   before:w-20  before:rounded-lg before:py-1 before:text-white before:font-bold"
-                )}
-              >
-                <Image
-                  className="mx-auto p-3"
-                  src={`https://bkduabhaudrkgjloqnck.supabase.co/storage/v1/object/public/e-card%20bucket/${card.thumbnail}`}
-                  width={200}
-                  height={200}
-                  alt={card.name}
-                />
-                <CardHeader>
-                  <CardTitle>{card.name}</CardTitle>
-                </CardHeader>
-
-                <CardFooter className="gap-2 flex flex-col lg:flex-row items-center justify-center md:w-full">
-                  <Link
-                    href="/preview/demo"
-                    target="_blank"
-                    className={cn(
-                      "w-full",
-                      buttonVariants({ variant: "secondary" })
-                    )}
-                  >
-                    Preview
-                  </Link>
-                  <Link
-                    href={`/user/create?design_id=${card.designId}`}
-                    className={cn(
-                      "w-full",
-                      buttonVariants({ variant: "default" })
-                    )}
-                  >
-                    Buy now
-                  </Link>
-                </CardFooter>
-              </Card>
-            ))}
-          </div>
-        </section>
-      </div>
+      <Suspense fallback={<HomeLoading />}>
+        <div className="flex flex-col md:flex-row">
+          <CatalogCard searchParams={searchParams} />
+        </div>
+      </Suspense>
     </main>
   );
 };
