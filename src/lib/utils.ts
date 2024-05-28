1bd32c785839e40f78d036bb6cbbeed5e26797c0
kyrool <kfitri559@gmail.com>
2024-05-28 18:25:31 +0800

    ♻️ add: format date in utils

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -2,6 +2,7 @@ import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 import { primaryFontsConst, secondaryFontsConst } from "./constant";
 import prisma from "../../prisma";
+import { format } from "date-fns";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -16,4 +17,12 @@ export function getFont(id: string | undefined) {
 
 export const checkDate = (date: Date | null | undefined) => {
   return new Date() >= new Date(date as Date);
+};
+
+export const formatDate = (date: Date) => {
+  const formattedDate = isNaN(date.getTime())
+    ? "Date not set"
+    : format(date, "dd/L/y");
+
+  return formattedDate;
 };
