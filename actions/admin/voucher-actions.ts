c31dafdebf0b269f112ea20ba7222f1920bf7e86
kyrool <kfitri559@gmail.com>
2024-06-01 23:17:49 +0800

    fix: performance issue and refactor code

--- actions/admin/voucher-actions.ts
+++ actions/admin/voucher-actions.ts
@@ -4,6 +4,71 @@ import { auth } from "@/auth";
 import { createVoucherFormSchema } from "../../schema/zod/admin-form";
 import { z } from "zod";
 import { revalidatePath } from "next/cache";
+import { voucherClaimSchema } from "../../schema/zod/ecard-form";
+
+export const voucherClaim = async (formData: FormData) => {
+  const values = Object.fromEntries(formData.entries());
+  const { voucher_code } = voucherClaimSchema.parse(values);
+  try {
+    const session = await auth();
+    if (!session) throw new Error("Unauthorized access");
+
+    const [voucher, existingClaim] = await Promise.all([
+      prisma.voucher.findUnique({
+        where: {
+          code: voucher_code,
+        },
+        select: {
+          code: true,
+          count_claims: true,
+          max_claims: true,
+          active: true,
+        },
+      }),
+      prisma.userVoucher.findUnique({
+        where: {
+          userId_voucherId: {
+            userId: session?.user?.id!,
+            voucherId: voucher_code!,
+          },
+        },
+      }),
+    ]);
+
+    if (!voucher || !voucher.active || existingClaim) {
+      return {
+        ok: false,
+        message: "Voucher either expired or doesn't exist",
+      };
+    }
+
+    //else create new record and update voucher count_claims
+    await prisma.$transaction([
+      prisma.userVoucher.create({
+        data: {
+          voucherId: voucher?.code!,
+          userId: session?.user?.id!,
+        },
+      }),
+      prisma.voucher.update({
+        where: {
+          code: voucher?.code!,
+        },
+        data: {
+          count_claims: {
+            increment: 1,
+          },
+          active:
+            voucher.count_claims + 1 >= voucher.max_claims ? false : undefined,
+        },
+      }),
+    ]);
+
+    return { ok: true, message: "Voucher claimed" };
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 export const uploadVoucher = async (
   formData: z.infer<typeof createVoucherFormSchema>
@@ -12,9 +77,8 @@ export const uploadVoucher = async (
     createVoucherFormSchema.parse(formData);
   try {
     const session = await auth();
-    if (!session) throw new Error("Unauthorized access");
-    if (session.user.role === "user") {
-      throw new Error("You have no right access");
+    if (!session || session.user.role === "user") {
+      throw new Error("Unauthorized access or insufficient permissions");
     }
     if (max_claims <= 0)
       return {
@@ -22,31 +86,22 @@ export const uploadVoucher = async (
         ok: false,
       };
 
-    const existingVoucher = await prisma.voucher.findUnique({
-      where: {
-        code,
-      },
-    });
+    const result = await prisma.$transaction(async (prisma) => {
+      const existingVoucher = await prisma.voucher.findUnique({
+        where: { code },
+      });
+      if (existingVoucher) {
+        return { message: "Voucher code already exists", ok: false };
+      }
 
-    if (existingVoucher) {
-      return {
-        message: "Voucher code already exists",
-        ok: false,
-      };
-    }
+      await prisma.voucher.create({
+        data: { code, description, max_claims },
+      });
 
-    await prisma.voucher.create({
-      data: {
-        code,
-        description,
-        max_claims,
-      },
+      revalidatePath("/auth/admin/vouchers");
+      return { message: "Voucher created successfully", ok: true };
     });
-    revalidatePath("/auth/admin/vouchers");
-    return {
-      message: "Voucher created successfully",
-      ok: true,
-    };
+    return result;
   } catch (error) {
     console.log(error);
   }
@@ -60,17 +115,18 @@ export const deleteVoucher = async (code: string) => {
   }
   try {
     // delete voucher
-    await prisma.voucher.delete({
-      where: {
-        code,
-      },
-    });
-    //delete user that claimed the voucher
-    await prisma.userVoucher.deleteMany({
-      where: {
-        voucherId: code,
-      },
-    });
+    await prisma.$transaction([
+      prisma.voucher.delete({
+        where: {
+          code,
+        },
+      }),
+      prisma.userVoucher.deleteMany({
+        where: {
+          voucherId: code,
+        },
+      }),
+    ]);
     revalidatePath("/auth/admin/vouchers");
     return {
       message: "Voucher deleted successfully",
@@ -78,5 +134,9 @@ export const deleteVoucher = async (code: string) => {
     };
   } catch (error) {
     console.log(error);
+    return {
+      message: "An error occurred",
+      ok: false,
+    };
   }
 };
