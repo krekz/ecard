da48c0913f8c505ae83f35e7befa84c11b4c3045
kyrool <kfitri559@gmail.com>
2024-06-01 23:20:43 +0800

    add voucher slug page

---
+++ src/app/(auth)/admin/vouchers/[slug]/page.tsx
@@ -0,0 +1,26 @@
+import VoucherForm from "@/components/admin/voucher-form";
+import Link from "next/link";
+import { notFound } from "next/navigation";
+import React from "react";
+import { LuArrowLeft } from "react-icons/lu";
+
+const Page = async ({
+  params,
+  searchParams,
+}: {
+  params: { slug: string };
+  searchParams: { [key: string]: string | undefined };
+}) => {
+  if (!searchParams.action || searchParams.action !== "create")
+    return notFound();
+  return (
+    <section className="relative col-span-6 xl:col-span-5 flex flex-col gap-10 justify-center items-center h-full py-10">
+      <Link href="/admin/vouchers" className=" p-3 rounded-md bg-purple-500">
+        <LuArrowLeft />
+      </Link>
+      <VoucherForm />
+    </section>
+  );
+};
+
+export default Page;
