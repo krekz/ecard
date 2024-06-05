b1aa8b1b3da1f4e10ac714e02eefad56d5c413b3
kyrool <kfitri559@gmail.com>
2024-06-06 02:30:05 +0800

    security: edit card authorize access

--- src/app/(root)/user/edit/page.tsx
+++ src/app/(root)/user/edit/page.tsx
@@ -18,29 +18,24 @@ const EditCardPage = async ({
   const cardId = searchParams.id;
 
   if (!session) redirect(`/api/auth/signin?`);
-  if (!cardId || typeof cardId !== "string") redirect("/user/cards");
 
   try {
+    if (!cardId || typeof cardId !== "string") redirect("/user/cards");
     const userCard = await GetCardDetail(cardId);
-    if (userCard?.data?.userId !== session?.user?.id) {
+    if (cardId !== userCard?.data?.id) throw new Error("Card not existed");
+    if (userCard?.data?.userId !== session?.user?.id)
       throw new Error("Unauthorized access");
-    }
+
     const disableEdit = checkDate(userCard?.data?.event?.date);
-    if (disableEdit) {
-      throw new Error("Cannot edit no more..");
-    }
+    if (disableEdit) throw new Error("Cannot edit no more..");
     const transformedData = {
       ...userCard?.data,
       heirs: Array.isArray(userCard?.data?.heirs) ? userCard?.data?.heirs : [],
     };
 
-    // if (userCard?.id !== session?.user?.id) {
-    //   throw new Error("Unauthorized access");
-    // }
-
     return (
       <>
-        <h1 className="font-bold text-5xl pt-10 text-center">Edit TEAcard</h1>
+        <h1 className="font-bold text-5xl pt-10 text-center">Edit TeaCard</h1>
         <FormNavbar />
         <div className="flex justify-center">
           <CardForm dataFromDB={transformedData} user={session?.user} />
